b3af26278cba0c99d02d367a9519e3c33813654c
jameelw05 <Jameelswalters@gmail.com>
2025-10-14 15:34:13 -0700

    Add files via upload

---
+++ QUIZ MAKEUP.py
@@ -0,0 +1,36 @@
+# Quiz 1 makeup 
+
+#Q5: What does "Hello + "World" evaluate to?
+print("Q5:", "Hello" + "World")
+#HelloWorld (no spaces)
+
+#Q7: What type of value is "42" in Python?
+print("Q7:", print(int(42)))
+#Integer
+
+#Q9: What type of value is 3.14 in Python?
+print("Q9:", type(3.14)._Float_)
+#Float
+
+#Q11: What is the result of 10 // 3 in Python?
+print("Q11:", 10//3)
+#3
+
+#Q12: What is the result of 2**3 in Python?
+print()
+#8
+
+#Q16: What does the function type(42) return?
+print ("Q16:", type(42))
+#<class 'int'>
+
+#Q17: What is the result of len("Spam, Spam, Spam")?
+print(len ("Spam, Spam, Spam")) 
+#16
+
+#Q18: What is the result of len("123") evaluate to?
+print("Q18:", int("123"))
+#123
+
+#Q20: which of the following functions calls will cause an error?
+print (len(42))
